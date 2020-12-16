7dd303a80d8856a2bc052d21643cc4818d00a69d
SG <sgng1@e.ntu.edu.sg>
2020-12-16 17:15:15 +0800

    minor change to styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import ChapterModal from "./components/chaptersModal";
 import Game from "./components/game";
 function App() {
   return (
-    <>
+    <main className="container my-3">
       <Game />
       <ChapterModal />
-    </>
+    </main>
   );
 }
 
--- src/components/game/index.jsx
+++ src/components/game/index.jsx
@@ -3,12 +3,10 @@ import Sprite from "./sprite";
 function Game() {
   const questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <div className="container my-3">
-      <div style={{ height: "60vh" }}>
-        {questions.map((qn) => (
-          <Sprite key={qn} />
-        ))}
-      </div>
+    <div>
+      {questions.map((qn) => (
+        <Sprite key={qn} />
+      ))}
     </div>
   );
 }
