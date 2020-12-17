a99427f89658cfb92b145bfdbff9470f9d99ec7d
SG <sgng1@e.ntu.edu.sg>
2020-12-17 10:34:05 +0800

    minor edits - modify qn, skip app.test.js

--- src/components/game/index.jsx
+++ src/components/game/index.jsx
@@ -3,20 +3,34 @@ import SpriteModal from "./spriteModal";
 import { useState } from "react";
 
 function Game() {
-  const questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const questions = [
+    {
+      title: 'What is displayed by `print("hello world")`?',
+      answer: "hello world",
+    },
+    { title: 2, answer: "" },
+    { title: 3, answer: "" },
+    { title: 4, answer: "" },
+    { title: 5, answer: "" },
+    { title: 6, answer: "" },
+    { title: 7, answer: "" },
+    { title: 8, answer: "" },
+    { title: 9, answer: "" },
+    { title: 10, answer: "" },
+  ];
   const [selectedQn, setSelectedQn] = useState("");
   const modalId = "spriteModal";
   return (
     <div>
       {questions.map((qn) => (
         <Sprite
-          key={qn}
+          key={qn.title}
           dataBsToggle="modal"
           dataBsTarget={`#${modalId}`}
           clickFn={() => setSelectedQn(qn)}
         />
       ))}
-      <SpriteModal modalId={modalId} modalContent={selectedQn} />
+      <SpriteModal modalId={modalId} modalContent={selectedQn.title} />
     </div>
   );
 }
