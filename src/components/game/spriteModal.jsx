752793273342387e1142582c1db5ad26e990e96d
SG <sgng1@e.ntu.edu.sg>
2020-12-17 14:07:29 +0800

    add input and mock checkanswer function

--- src/components/game/spriteModal.jsx
+++ src/components/game/spriteModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function SpriteModal(props) {
   const { modalId, modalContent } = props;
@@ -6,11 +6,16 @@ function SpriteModal(props) {
     // eslint-disable-next-line
     eval(
       `
-      var popoverTriggerList = Array.from(document.querySelectorAll('.btn.btn-primary.popoverclass'));
+      var popoverTriggerList = Array.from(document.querySelectorAll('.btn.btn-outline-primary.popoverclass'));
       var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {return new bootstrap.Popover(popoverTriggerEl)})
       `
     );
   }, []);
+  const [answer, setAnswer] = useState("answer");
+  const checkAnswer = () =>
+    window.alert(
+      modalContent.answer.includes(answer) ? "answer correct" : "wrong answer"
+    );
   return (
     <div
       className="modal fade"
@@ -24,7 +29,29 @@ function SpriteModal(props) {
           <div className="modal-header">
             <h5 className="modal-title">{modalContent.title}</h5>
           </div>
-          <div className="modal-body">{modalContent.question}</div>
+          <div className="modal-body">
+            {modalContent.question}
+            <hr />
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Answer"
+                aria-label="Answer"
+                aria-describedby={modalContent.title}
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+              />
+              <button
+                className="btn btn-primary"
+                type="button"
+                id={modalContent.title}
+                onClick={checkAnswer}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
           <div
             className="modal-footer"
             style={{ justifyContent: "space-between" }}
@@ -32,7 +59,7 @@ function SpriteModal(props) {
             {/* eslint-disable-next-line */}
             <a
               tabIndex="0"
-              className="btn btn-primary popoverclass"
+              className="btn btn-outline-primary popoverclass"
               role="button"
               data-bs-toggle="popover"
               data-bs-trigger="focus"
