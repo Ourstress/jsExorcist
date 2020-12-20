7a6f6da1f416bae2bdd3ac4678eaf4388761cd71
SG <sgng1@e.ntu.edu.sg>
2020-12-20 18:34:03 +0800

    add close button & animation to story component

--- src/components/story/index.jsx
+++ src/components/story/index.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react";
 import waves from "../../assets/waves.png";
+import { useTransition, animated } from "react-spring";
 
 function Story() {
-  return (
-    <div className="card text-black">
-      <img
-        src={waves}
-        className="card-img"
-        alt="waves"
-        style={{ objectFit: "cover", opacity: 0.2, overflow: "hidden" }}
-      />
-      <div className="card-img-overlay">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">hello.</p>
-      </div>
-    </div>
+  const [storyDisplay, toggleStoryDisplay] = useState(true);
+  const transitions = useTransition(storyDisplay, null, {
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+  return transitions.map(
+    ({ item, key, props }) =>
+      item && (
+        <animated.div
+          className="card text-black"
+          style={{ maxHeight: "25vh", ...props }}
+          key={key}
+        >
+          <img
+            src={waves}
+            className="card-img"
+            alt="waves"
+            style={{ objectFit: "cover", opacity: 0.2, overflow: "hidden" }}
+          />
+          <div className="card-img-overlay">
+            <section
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <h5 className="card-title">Card title</h5>
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={() => toggleStoryDisplay(!storyDisplay)}
+              ></button>
+            </section>
+            <p className="card-text">hello.</p>
+          </div>
+        </animated.div>
+      )
   );
 }
 
