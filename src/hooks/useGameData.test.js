c0ca7079f7d437f49c8b513da29fb42b68310141
SG <sgng1@e.ntu.edu.sg>
2020-12-23 12:49:24 +0800

    added useGameData tests

--- src/hooks/useGameData.test.js
+++ src/hooks/useGameData.test.js
@@ -7,13 +7,44 @@ const chapter1 = {
   questions: [{ id: 2, title: 2, question: "", answer: "", hint: "a" }],
 };
 
+const chapter2 = {
+  name: "chapter 2",
+  story: "hey yoyo",
+  questions: [{ id: 2, title: 2, question: "", answer: "", hint: "a" }],
+};
+
 const gameState = {
   overall: {
     currentChapter: 0,
   },
 };
-const gameData = new Map([["chapter 1", chapter1]]);
+const gameData = new Map([
+  ["chapter 1", chapter1],
+  ["chapter 2", chapter2],
+]);
 test("return first chapter as currentChapter by default", () => {
   const { result } = renderHook(() => useGameData(gameData, gameState));
   expect(result.current[0]).toBe(chapter1);
+});
+
+test("takes gameState currentChapter into account", () => {
+  const currentChapter2GameState = {
+    overall: {
+      currentChapter: 1,
+    },
+  };
+  const { result } = renderHook(() =>
+    useGameData(gameData, currentChapter2GameState)
+  );
+  expect(result.current[0]).toBe(chapter2);
+});
+
+test("setCurrentChapterIndex causes currentChapterIndex to change", () => {
+  const { result } = renderHook(() => useGameData(gameData, gameState));
+  expect(result.current[1]).toBe(0);
+  const newChapterIndex = 1;
+  act(() => {
+    result.current[2](newChapterIndex);
+  });
+  expect(result.current[1]).toBe(newChapterIndex);
 });
