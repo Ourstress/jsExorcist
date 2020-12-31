23ed3f25899ae7065d33b9075beca612a07a035b
SG <sgng1@e.ntu.edu.sg>
2020-12-31 13:47:14 +0800

    solve object copying bug in test cases

--- src/hooks/useGameState.test.js
+++ src/hooks/useGameState.test.js
@@ -2,74 +2,61 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import useGameState from "./useGameState";
 import { QUESTIONSTATUS } from "../data/constants";
 
-const chapter1 = {
+const originalChapter1 = {
   name: "chapter 1",
   story: "Welcome",
   questions: [
-    {
-      id: 2,
-      title: "chapter 1 question",
-      question: "",
-      answer: ["", "correct answer"],
-      hint: "a",
-    },
+    { id: 2, title: "chapter 1 question", question: "", answer: ["", "correct answer"], hint: "a" },
   ],
 };
 
-const chapter2 = {
-  name: "chapter 2",
-  story: "hey yoyo",
-  questions: [
-    { id: 2, title: "chapter 2 question", question: "", answer: "", hint: "a" },
-  ],
+const originalGameState = {
+  overall: { currentChapter: 0 },
+  chapters: { "chapter 1": { 1: { status: "in progress" } } },
 };
 
-const gameState = {
-  overall: {
-    currentChapter: 0,
-  },
-  chapters: {
-    "chapter 1": {
-      1: { status: "in progress" },
-    },
-  },
-};
+let result;
+let chapter1;
+let gameState;
+beforeEach(() => {
+  chapter1 = JSON.parse(JSON.stringify(originalChapter1));
+  gameState = JSON.parse(JSON.stringify(originalGameState));
+  const renderedHook = renderHook(() => useGameState(chapter1, gameState));
+  result = renderedHook.result;
+});
 
 test("return questions that combines gameState and gameData", () => {
-  const { result } = renderHook(() => useGameState(chapter1, gameState));
   const updatedQuestion1 = {
-    ...chapter1.questions[0],
+    ...originalChapter1.questions[0],
     status: QUESTIONSTATUS.pending,
   };
   expect(result.current[0]).toStrictEqual([updatedQuestion1]);
 });
 
 test("checkanswer changes status to in progress if answer wrong", () => {
-  const { result } = renderHook(() => useGameState(chapter1, gameState));
   act(() => {
     result.current[1](chapter1.questions[0], "wrong");
   });
   const updatedQuestion1 = {
-    ...chapter1.questions[0],
+    ...originalChapter1.questions[0],
     status: QUESTIONSTATUS.trying,
   };
   expect(result.current[0]).toStrictEqual([updatedQuestion1]);
 });
 
 test("checkanswer checks for alternate answers", () => {
-  const { result } = renderHook(() => useGameState(chapter1, gameState));
   act(() => {
     result.current[1](chapter1.questions[0], "correct answer");
   });
   const updatedQuestion1 = {
-    ...chapter1.questions[0],
+    ...originalChapter1.questions[0],
     status: QUESTIONSTATUS.correct,
   };
+
   expect(result.current[0]).toStrictEqual([updatedQuestion1]);
 });
 
 test("readyNextChapter changes to true when all questions correct", () => {
-  const { result } = renderHook(() => useGameState(chapter1, gameState));
   expect(result.current[2]).toBe(false);
 
   act(() => {
