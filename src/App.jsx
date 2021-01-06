820b95268cbfbe6b097df07cd225bb0ff6112cbc
SG <sgng1@e.ntu.edu.sg>
2021-01-06 21:26:07 +0800

    resolve multiple firebase initialisation error

--- src/App.jsx
+++ src/App.jsx
@@ -49,9 +49,12 @@ function App() {
       authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
       projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     };
-    firebase.initializeApp(firebaseConfig);
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
     dbRef.current = firebase.firestore();
   }, []);
+
   // trigger popup if close tab
   useEffect(() => {
     const inProd = process.env.NODE_ENV !== "development";
